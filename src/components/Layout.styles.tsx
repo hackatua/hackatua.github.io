f11e96754988dd8e4720151bb54eafd1538d5f1c
Hackatua <hackatua@gmail.com>
2022-01-02 12:03:10 +0100

    feat: adds width to layout

---
+++ src/components/Layout.styles.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const StyledLayout = styled.div`
+  width: 90%;
+  max-width: 1000px;
+  margin: 0 auto;
+`
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,10 @@
 import React from "react"
 import { GlobalStyles } from "./GlobalStyles"
+import { StyledLayout } from "./Layout.styles"
 
 export const Layout: React.FC = ({ children }) => (
-  <>
+  <StyledLayout>
     <GlobalStyles />
     {children}
-  </>
+  </StyledLayout>
 )
