67b3fad2510bd4d7cc77647fb32052796b2b7df0
Hackatua <hackatua@gmail.com>
2022-01-02 13:17:40 +0100

    feat: adds wiki page

---
+++ src/pages/wiki.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Layout } from "../components/Layout"
+
+const WikiPage: React.VFC = () => {
+  return (
+    <Layout>
+      <div>Wiki Page!</div>
+    </Layout>
+  )
+}
+
+export default WikiPage
