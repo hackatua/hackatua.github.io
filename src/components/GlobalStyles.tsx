beb1d9f914b6f48dcbe139bf1b9e9332eedac0e0
Hackatua <hackatua@gmail.com>
2022-01-02 12:29:24 +0100

    feat: adds Hack font

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -1,11 +1,20 @@
 import { createGlobalStyle } from "styled-components"
-import background from '../images/background.png'
+import background from "../images/background.png"
+import hackRegularWebFontWoff from "../fonts/hack-regular-webfont.woff"
+import hackRegularWebFontWoff2 from "../fonts/hack-regular-webfont.woff2"
 
 export const GlobalStyles = createGlobalStyle`
+    @font-face {
+        font-family: 'Hack';
+        src: url(${hackRegularWebFontWoff2}) format('woff2'),
+             url(${hackRegularWebFontWoff}) format('woff');
+    }
+    
     body {
         margin: 0;
         padding: 0;
         background: url(${background});
         color: #eaeaea;
+        font-family: Hack;
     }
 `
