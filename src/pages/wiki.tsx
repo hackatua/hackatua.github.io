880eb6f81790396f2367554a6e89cfbf4a709e87
Hackatua <hackatua@gmail.com>
2022-01-02 19:19:51 +0100

    feat: lists wiki content in wiki main page

--- src/pages/wiki.tsx
+++ src/pages/wiki.tsx
@@ -1,12 +1,51 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 import { Layout } from "../components/Layout"
 
-const WikiPage: React.VFC = () => {
+interface Props {
+  data: {
+    allMarkdownRemark: {
+      nodes: {
+        fields: { slug: string }
+        id: string
+        frontmatter: {
+          title?: string
+        }
+      }[]
+    }
+  }
+}
+
+const WikiPage: React.VFC<Props> = ({ data }) => {
   return (
     <Layout>
-      <div>Wiki Page!</div>
+      <h2>Wiki Page!</h2>
+      <h3>Content</h3>
+      <ul>
+        {data.allMarkdownRemark.nodes.map(node => (
+          <li key={node.id}>
+            <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+          </li>
+        ))}
+      </ul>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(filter: { fields: { contentType: { eq: "wiki" } } }) {
+      nodes {
+        fields {
+          slug
+        }
+        id
+        frontmatter {
+          title
+        }
+      }
+    }
+  }
+`
 
 export default WikiPage
