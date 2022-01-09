68da4318d4dbb1537cb9a15ecf1e8c4e36290911
Hackatua <hackatua@gmail.com>
2022-01-09 10:38:07 +0100

    feat: applies reset to css styles

--- src/components/GlobalStyles/GlobalStyles.tsx
+++ src/components/GlobalStyles/GlobalStyles.tsx
@@ -2,8 +2,11 @@ import { createGlobalStyle } from "styled-components"
 import background from "../../images/background.png"
 import hackRegularWebFontWoff from "../../fonts/hack-regular-webfont.woff"
 import hackRegularWebFontWoff2 from "../../fonts/hack-regular-webfont.woff2"
+import { resetStyles } from "./reset-styles"
 
 export const GlobalStyles = createGlobalStyle`
+    ${resetStyles};
+
     @font-face {
         font-family: 'Hack';
         src: url(${hackRegularWebFontWoff2}) format('woff2'),
