5b1be7de3c6c63d1931fbe0f6bc160d569403417
Hackatua <hackatua@gmail.com>
2022-01-14 17:47:22 +0100

    feat: adds ssr to styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,5 +20,11 @@ module.exports = {
         plugins: ["gatsby-remark-prismjs"],
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
   ],
 }
