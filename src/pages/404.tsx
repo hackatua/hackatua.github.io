33b278419bd8eb0e062955b5e372c588d2450015
Hackatua <hackatua@gmail.com>
2022-01-15 10:51:07 +0100

    feat: adds basic 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { PageProps } from "gatsby"
+
+const Page404: React.VFC<PageProps> = ({ location }) => (
+  <p>404: no such file or directory: {location.pathname}</p>
+)
+
+export default Page404
