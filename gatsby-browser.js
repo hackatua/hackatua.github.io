97e639bdf84aa1231c12ca1c4756cbc3ed87af54
Hackatua <hackatua@gmail.com>
2022-01-15 10:07:38 +0100

    refactor: moves GlobalStyles to gatsby-browser wrapRootElement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,13 @@
 import React from "react"
 import "prismjs/themes/prism-tomorrow.css"
 import { Layout } from "./src/components/Layout"
+import { GlobalStyles } from "./src/components/GlobalStyles"
 
 export const wrapPageElement = ({ element }) => <Layout>{element}</Layout>
+
+export const wrapRootElement = ({ element }) => (
+  <>
+    <GlobalStyles />
+    {element}
+  </>
+)
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,9 @@
 import React from "react"
-import { GlobalStyles } from "../GlobalStyles"
 import { Header } from "../Header"
 import { StyledLayout } from "./Layout.styles"
 
 export const Layout: React.FC = ({ children }) => (
   <>
-    <GlobalStyles />
     <Header>Hackatua</Header>
     <StyledLayout>{children}</StyledLayout>
   </>
