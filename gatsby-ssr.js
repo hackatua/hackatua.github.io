f38922de9a6d5bfd7c92b25a2cc33f17f450f785
Hackatua <hackatua@gmail.com>
2022-01-15 13:32:15 +0100

    chore: adds gatsby-ssr

---
+++ gatsby-ssr.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { ThemeProvider } from "styled-components"
+
+import { Layout } from "./src/components/Layout"
+import { GlobalStyles } from "./src/components/GlobalStyles"
+import { theme } from "./src/theme"
+
+import "prismjs/themes/prism-tomorrow.css"
+
+export const wrapPageElement = ({ element }) => <Layout>{element}</Layout>
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    {element}
+  </ThemeProvider>
+)
