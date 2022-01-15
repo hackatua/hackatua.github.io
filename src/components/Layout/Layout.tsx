02f48feab23b13bbfd3ea25eb36c5952b16ea225
Hackatua <hackatua@gmail.com>
2022-01-15 10:01:47 +0100

    refactor: moves Layout files to a folder

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import { GlobalStyles } from "./GlobalStyles"
-import { Header } from "./Header"
+import { GlobalStyles } from "../GlobalStyles"
+import { Header } from "../Header"
 import { StyledLayout } from "./Layout.styles"
 
 export const Layout: React.FC = ({ children }) => (
